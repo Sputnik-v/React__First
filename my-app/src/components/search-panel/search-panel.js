19bc497a1e65dc9b482dcc2589f2de478a6b82ab
Igor Tronin <igortronin2705@gmail.com>
2022-02-08 12:25:02 +0400

    Application structure

---
+++ my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,14 @@
+import './search-panel.css';
+
+const SearhPanel = () => {
+
+    return (
+        <input 
+            type="text" 
+            className="form-control search-input"
+            placeholder="Найти сотрудника"
+            />
+    )
+}
+
+export default SearhPanel;
